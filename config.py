dcf809bb3164a87997d04e452b99b7c25cd497e2
J G Prasad <jgprasa@publicisgroupe.net>
2021-04-09 12:19:50 +0530

    saved code

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c91ntxfgzk6z.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "add-employe"
+customregion = "us-east-1"
